aa4a344bf75cc1c609ae1c605c3aa18464c7d364
Birgül Özhan <birgulozhan9@gmail.com>
2025-06-26 23:50:12 +0300

    parallaz alanı kodlandı.

---
+++ src/components/services/Services.jsx
@@ -0,0 +1,41 @@
+import "./services.scss";
+import { motion } from "framer-motion";
+
+const Services = () => {
+  return (
+    <motion.div className="services">
+      <motion.div className="textContainer">
+        <p>
+            I focus on helping your brand grow 
+            <br/> and move forward</p>
+        <hr/>
+      </motion.div>
+      <motion.div className="titleContainer">
+        <div className="title">
+            <img src="/people.webp" alt="baslik gorsel" />
+            <h1>
+                <b>Unique</b> Ideas
+            </h1>
+        </div>
+
+        <div className="title">
+            <h1>
+                <b>For Your</b> Business
+            </h1>
+            <button>WHAT WE DO?</button>
+        </div>
+      </motion.div>
+      <motion.div className="listContainer">
+        <div className="box">
+            <h2>Branding</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum excepturi optio aliquid libero repudiandae blanditiis placeat asperiores maxime sit laudantium autem pariatur laborum, cum harum, cumque fugiat, necessitatibus aperiam eius.
+            </p>
+            <button>Go</button>
+        </div>
+      </motion.div>
+    </motion.div>
+  )
+}
+
+export default Services
