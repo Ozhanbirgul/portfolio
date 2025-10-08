d26721b3407452675e0c03311787d90eb8c5adf1
Birgül Özhan <birgulozhan9@gmail.com>
2025-10-08 10:49:40 +0300

    logo alanı düzenlendi.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,46 +1,32 @@
-import React, { useState } from 'react';
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { TbFileCv } from "react-icons/tb";
-import logo from "../images/logo.png"
-import { GiHamburgerMenu } from "react-icons/gi";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 
 const Header = () => {
-
   return (
-    <>
-        <header className='relative p-2 h-24 overflow-visible bg-black text-white'>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-4 col-md-4 col-6 relative z-10">
-                        <a href="">
-                            <img src={logo} className="rounded-full w-30 h-30 shadow-lg" alt="" />
-                        </a>
-                    </div>
-                    <div className="col-lg-8 col-md-8 col-6 flex justify-end ml-auto">
-                        <ul className="navbar-box flex items-center justify-center gap-x-4 mb-4">
-                            <li>
-                                <a href="">
-                                    <FaGithub className='text-3xl'/>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="">
-                                    <FaLinkedin className='text-3xl'/>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="">
-                                    <TbFileCv className='text-3xl'/>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </header>
-    </>
-  )
-}
+    <AppBar sx={{ backgroundColor: "black" }}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography
+          variant="h6"
+          sx={{
+            fontWeight: "bold",
+            fontSize: "1.5rem",
+            letterSpacing: 2,
+            cursor: "pointer",
+            ml: 3
+          }}
+        >
+          &lt;BD /&gt;
+        </Typography>
 
-export default Header
+        <Box>İkon Alanı</Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,88 +1,121 @@
-import React from 'react';
+import React from "react";
 import serRezistansLogo from "../images/header-logo.png";
 import tlgLogo from "../images/tlg-logo.png";
-import profile from "../images/profile.jpg"
+import profile from "../images/profile.jpg";
 
 const Hero = () => {
-    return (
-        <div className="hero-section p-5 bg-stone-300">
-            <div className='container '>
-                <div className="row">
-                    <div className="col-lg-4">
-                        <div className="profile-card bg-gray-600  flex flex-col items-center rounded-[20px] p-4">
-                            <img src={profile} className='profile w-60 h-60 object-cover mb-4' alt="profile image" />
-                            <div className="content">
-                                <div className="title text-white text-3xl text-center">Birgül Özhan</div>
-                                <div className="job-title text-center text-gray-400">Front-end Developer</div>
-                                <div className="short-text text-white text-center mt-2 text-sm">
-                                    Sakarya Üniversitesi Matematik Bölümü mezunuyum. 1 yıldır bir ajans bünyesinde Front-end Developer olarak çalışıyor, kullanıcı odaklı arayüzler geliştiriyorum.
-                                </div>
-                                <ul className='p-3'>
-                                    <li className='text-white'>
-                                        Mail: <a href="mailto:birgulozhan9@gmail.com" className='underline' target="_blank">birgulozhan9@gmail.com</a>
-                                    </li>
-                                    <li className='text-white'>
-                                        Linkedin: <a href="https://www.linkedin.com/in/birgul-ozhan00/" className='underline' target="_blank">https://www.linkedin.com/in/birgul-ozhan00/</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-lg-8 flex justify-center">
-                        <div className="relative pl-1 border-l-2 border-gray-600">
+  return (
+    <div className="hero-section p-5 bg-stone-300">
+      <div className="container ">
+        <div className="row">
+          <div className="col-lg-4">
+            <div className="profile-card bg-gray-600  flex flex-col items-center rounded-[20px] p-4">
+              <img
+                src={profile}
+                className="profile w-40 h-40 mb-4 object-cover"
+                alt="profile image"
+              />
 
-                            {/* Proje Kutusu 1 */}
-                            <div href="https://www.serrezistans.com/" className="relative mb-10 w-[350px]">
-                                {/* Nokta */}
-                                <div className="absolute -left-3 top-2 w-4 h-4 rounded-full bg-blue-500 border-2 border-white"></div>
-
-                                {/* Kutunun kendisi */}
-                                <div 
-                                className="bg-white rounded-2xl shadow-md overflow-hidden transition hover:shadow-xl hover:scale-[1.01] ml-4 max-w-[600px]">
-                                    <img
-                                        src={serRezistansLogo}
-                                        alt="project logo"
-                                        className="w-full h-48 object-contain bg-gray-600 p-4"
-                                    />
-                                    <div className="p-4">
-                                        <div className="text-xl font-semibold mb-2">Ser Rezistans</div>
-                                        <p className="text-gray-600 text-sm">
-                                            Click to Peak bünyesinde front-end developer olarak çalıştığım dönemde Laravel Blade yapısı kullanarak CMS sistemiyle ön yüzünü kodladım.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            {/* Proje Kutusu 2 */}
-                            <div className="relative mb-10 w-[350px]">
-                                <div className="absolute -left-3 top-2 w-4 h-4 rounded-full bg-blue-500 border-2 border-white"></div>
-
-                                <a
-                                    href="https://www.tlgmaterials.com/"
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    className="block bg-white rounded-2xl shadow-md overflow-hidden transition hover:shadow-xl hover:scale-[1.01] ml-4 max-w-[600px]"
-                                >
-                                    <img
-                                        src={tlgLogo}
-                                        alt="project logo"
-                                        className="w-full h-48 object-contain bg-gray-600 p-4"
-                                    />
-                                    <div className="p-4">
-                                        <div className="text-xl font-semibold mb-2">TLG Materials</div>
-                                        <p className="text-gray-600 text-sm">
-                                            Click to Peak bünyesinde front-end developer olarak çalıştığım dönemde Laravel Blade yapısı kullanarak CMS sistemiyle ön yüzünü kodladım.
-                                        </p>
-                                    </div>
-                                </a>
-                            </div>
-
-                        </div>
-                    </div>
+              <div className="content">
+                <div className="title text-white text-3xl text-center">
+                  Birgül Demirden
                 </div>
+                <div className="job-title text-center text-gray-400">
+                  Front-end Developer
+                </div>
+                <div className="short-text text-white text-center mt-2 text-sm">
+                  Sakarya Üniversitesi Matematik Bölümü mezunuyum. 1 yıldır bir
+                  ajans bünyesinde Front-end Developer olarak çalışıyor,
+                  kullanıcı odaklı arayüzler geliştiriyorum.
+                </div>
+                <ul className="p-3">
+                  <li className="text-white">
+                    Mail:{" "}
+                    <a
+                      href="mailto:birgulozhan9@gmail.com"
+                      className="underline"
+                      target="_blank"
+                    >
+                      birgulozhan9@gmail.com
+                    </a>
+                  </li>
+                  <li className="text-white">
+                    Linkedin:{" "}
+                    <a
+                      href="www.linkedin.com/in/birgül-demirden"
+                      className="underline"
+                      target="_blank"
+                    >
+                      https://www.linkedin.com/in/birgul-ozhan00/
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
-        </div>
-    )
-}
+          </div>
+          <div className="col-lg-8 flex justify-center">
+            <div className="relative pl-1 border-l-2 border-gray-600">
+              {/* Proje Kutusu 1 */}
+              <div
+                href="https://www.serrezistans.com/"
+                className="relative mb-10 w-[350px]"
+              >
+                {/* Nokta */}
+                <div className="absolute -left-3 top-2 w-4 h-4 rounded-full bg-blue-500 border-2 border-white"></div>
 
-export default Hero
+                {/* Kutunun kendisi */}
+                <div className="bg-white rounded-2xl shadow-md overflow-hidden transition hover:shadow-xl hover:scale-[1.01] ml-4 max-w-[600px]">
+                  <img
+                    src={serRezistansLogo}
+                    alt="project logo"
+                    className="w-full h-48 object-contain bg-gray-600 p-4"
+                  />
+                  <div className="p-4">
+                    <div className="text-xl font-semibold mb-2">
+                      Ser Rezistans
+                    </div>
+                    <p className="text-gray-600 text-sm">
+                      Click to Peak bünyesinde front-end developer olarak
+                      çalıştığım dönemde Laravel Blade yapısı kullanarak CMS
+                      sistemiyle ön yüzünü kodladım.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              {/* Proje Kutusu 2 */}
+              <div className="relative mb-10 w-[350px]">
+                <div className="absolute -left-3 top-2 w-4 h-4 rounded-full bg-blue-500 border-2 border-white"></div>
+
+                <a
+                  href="https://www.tlgmaterials.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="block bg-white rounded-2xl shadow-md overflow-hidden transition hover:shadow-xl hover:scale-[1.01] ml-4 max-w-[600px]"
+                >
+                  <img
+                    src={tlgLogo}
+                    alt="project logo"
+                    className="w-full h-48 object-contain bg-gray-600 p-4"
+                  />
+                  <div className="p-4">
+                    <div className="text-xl font-semibold mb-2">
+                      TLG Materials
+                    </div>
+                    <p className="text-gray-600 text-sm">
+                      Click to Peak bünyesinde front-end developer olarak
+                      çalıştığım dönemde Laravel Blade yapısı kullanarak CMS
+                      sistemiyle ön yüzünü kodladım.
+                    </p>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
